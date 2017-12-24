7ce8e1d381595bbce337a2e7f265dda2cf087d85
TaeHyoungKwon <kth5604@gmail.com>
2017-12-25 02:42:40 +0900

    Retrieve API View aka Detail View

--- src/posts/api/serializers.py
+++ src/posts/api/serializers.py
@@ -2,7 +2,19 @@ from rest_framework.serializers import ModelSerializer
 
 from posts.models import Post
 
-class PostSerializer(ModelSerializer):
+class PostListSerializer(ModelSerializer):
+    class Meta:
+        model = Post
+        fields = [
+            'id',
+            'title',
+            'slug',
+            'content',
+            'publish',
+        ]
+
+
+class PostDetailSerializer(ModelSerializer):
     class Meta:
         model = Post
         fields = [
--- src/posts/api/urls.py
+++ src/posts/api/urls.py
@@ -2,10 +2,12 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from .views import (
-    PostListAPIView
+    PostDetailAPIView,
+	PostListAPIView,
 	)
 
 urlpatterns = [
 	url(r'^$', PostListAPIView.as_view(), name='list'),
+	url(r'^(?P<slug>[\w-]+)/$', PostDetailAPIView.as_view(), name='detail'),
 
 ]
